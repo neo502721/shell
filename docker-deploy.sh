3235f87d59c9267833c37ee122c0e284116484d4
zhuguangming <zhuguangming@cmsr.chinamobile.com>
2020-04-21 16:02:56 +0800

    ubuntu deploy docker

---
+++ docker-deploy.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+
+
+sudo apt-get install docker-ce=5:18.09.0~3-0~ubuntu-xenial docker-ce-cli=5:18.09.0~3-0~ubuntu-xenial containerd.io
+#sudo apt-get update
+#sudo apt-get install docker-ce docker-ce-cli containerd.io
+
